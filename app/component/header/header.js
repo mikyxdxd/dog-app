8a59f16a427f6b261ababc2865f3d30b109c63b0
shilianghong <shiliang.hong01@gmail.com>
2016-07-11 11:50:47 -0700

    -menu_bar & menu_page animation

--- app/component/header/header.js
+++ app/component/header/header.js
@@ -1,6 +1,6 @@
 angular.module('app.header', []).directive('header', [()=>{
-        
-        
+
+
         require('./header.scss');
     return {
 
@@ -10,29 +10,74 @@ angular.module('app.header', []).directive('header', [()=>{
 
             scope.barAnimation = function(){
 
-                $('#top_bar').animate({top:'-3px'},300,'easeOutQuart');
-                $('#bottom_bar').animate({bottom:'-3px'},300,'easeOutQuart');
+                $('#top_bar').stop(true,false).animate({top:'-3px'},200,'easeOutQuart');
+                $('#bottom_bar').stop(true,false).animate({bottom:'-3px'},200,'easeOutQuart');
+
+            }
+
+
+            scope.barCross = ()=>{
+
+
+                $('#top_bar').addClass('top_cross');
+                $('#bottom_bar').addClass('bottom_cross');
 
             }
 
             scope.barAnimationDown = function(){
 
 
-                $('#top_bar').animate({top:'0px'},300,'easeOutQuart');
-                $('#bottom_bar').animate({bottom:'0px'},300,'easeOutQuart');
+                $('#top_bar').stop(true,false).animate({top:'0px'},200,'easeOutQuart');
+                $('#bottom_bar').stop(true,false).animate({bottom:'0px'},200,'easeOutQuart');
 
             }
 
             scope.growMenuPage = function(){
 
-                $('#menupage').animate({'width':'100vw'},500,()=>{
+                if( $('#top_bar').hasClass('top_cross')){
+
+                    $('#top_bar').removeClass('top_cross');
+                    $('#bottom_bar').removeClass('bottom_cross');
+                    $('#top_bar').css('background','black');
+                    $('#bottom_bar').css('background','black');
+                    $('#menupage .item').each(function(i,e){
+
+                        $(e).stop(true,false).delay(i*200).animate({opacity:'0'},300);
+
+                    })
+
+                    $('#menupage').stop(true,false).delay(1000).animate({'width':'0vw'},500,'easeOutQuint');
+
+
+                }else{
+
+
+
+
+                $('#top_bar').addClass('top_cross');
+                $('#bottom_bar').addClass('bottom_cross');
+                $('#header #menu_container').stop(true,false).delay(500).animate({opacity:'0'},200,()=>{
+
+                    $('#menupage').stop(true,false).animate({'width':'100vw'},500,'easeOutQuint',()=>{
+                    $('#top_bar').css('background','white');
+                    $('#bottom_bar').css('background','white');
+
+                    $('#header #menu_container').stop(true,false).animate({opacity:'1'},200);
 
                     $('#menupage .item').each(function(i,e){
 
-                        $(e).delay(i*200).animate({opacity:'1'},300)
+                        $(e).stop(true,false).delay(i*200).animate({opacity:'1'},300);
+
                     })
 
                 })
+
+
+            });
+
+
+
+                }
 
             }
         }
